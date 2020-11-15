0f095de3ef55182840abdb541cc4d8339d0e40c5
pillemer <pillemer@gmail.com>
2020-11-15 19:31:11 +1000

    started creating a simple layout

--- restaurantPage/dist/main.js
+++ restaurantPage/dist/main.js
@@ -1,1 +1,1 @@
-document.body.appendChild(function(){const n=document.createElement("div");return n.innerHTML="This is going to be a restaurant landing page",n}());
+document.body.appendChild(function(){const n=document.createElement("div");return n.innerHTML="This is going to be a footer with contact links and atribbution",n}());
--- restaurantPage/src/index.js
+++ restaurantPage/src/index.js
@@ -1,10 +1,9 @@
 
 
-
 function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = 'This is going to be a restaurant landing page';
+    element.innerHTML = 'This is going to be a footer with contact links and atribbution';
     return element;
 }
 
