dca0e41425e4347412da743a25c5d87de5eff0e2
pillemer <pillemer@gmail.com>
2020-11-17 15:02:58 +1000

    updated seprate tab pages and moved img files

--- restaurantPage/src/index.js
+++ restaurantPage/src/index.js
@@ -1,23 +1,3 @@
-// import blurb from './homepage';
+import homepage from './homepage';
 
-const navbarAndLogo = (() => {
-    const content = document.querySelector('#content');
-    // create the navbar
-    const navbar = document.createElement('div');
-    navbar.id = 'navigation';
-    const navbuttons = ['HOME', 'PLATES', 'ABOUT', 'CONTACT'];
-    for (let i=0; i<navbuttons.length; i++) {
-        const button = document.createElement('button');
-        button.id = navbuttons[i].toLowerCase();
-        button.textContent = navbuttons[i];
-        navbar.appendChild(button);
-    }
-    content.appendChild(navbar);
-
-    const logo = "( PLATE > SWITCH )";
-    const logoDiv = document.createElement('div');
-    logoDiv.id = 'logo';
-    logoDiv.textContent = logo;
-    content.appendChild(logoDiv);
-})();
-
+homepage();
